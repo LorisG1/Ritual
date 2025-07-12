1d82e93c37edaec7eb247b00c0a477d06cd6b3aa
LorisG1 <104369434+LorisG1@users.noreply.github.com>
2025-07-12 13:30:50 +0300

    Create ritual.sh

---
+++ ritual.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+PROJECT_NAME="Ritual"
+
+
+# Install denepdencies
+source <(wget -O- 'https://raw.githubusercontent.com/CBzeek/Nodes/main/!tools/server-prepare.sh')
+
+
+# Install Docker
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/ritual/docker.sh')
+docker run hello-world
+
+# Install foundry
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/ritual/foundry.sh')
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Installing $PROJECT_NAME node... \e[0m" && sleep 1
+echo ""
+# Clone Ritual's repository
+cd $HOME
+git clone https://github.com/ritual-net/infernet-container-starter
+cd infernet-container-starter
+sed -i 's/docker compose/docker-compose/' $HOME/infernet-container-starter/Makefile
+
+
+# Configure node
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/ritual/configure-node.sh')
+
+
+# Deploy container
+#project=hello-world make deploy-container
+docker-compose -f $HOME/infernet-container-starter/deploy/docker-compose.yaml up -d
+
+
+# Check docker
+docker container ls
